bc440b2e51a898b792ddf3e6a3daa514d26b67e2
chrscalv <chrscalvn@gmail.com>
2021-10-28 20:00:42 +0700

    updated gitignore create style dir and pages file

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../assets/styles/index.css'
+
+function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page)
+  return getLayout(<Component {...pageProps} />)
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Button from "../components/Button/Button";
+
+export default function home()
+{
+    return(
+    <>
+        <Button type="submit" size="large" label="test"  />
+    </>)
+}
