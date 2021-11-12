f8b595f65c144e66729ce50b03855665d8acf6da
chrscalv <chrscalvn@gmail.com>
2021-11-12 11:33:23 +0700

    add card subtitle to index component

--- components/index.js
+++ components/index.js
@@ -1,14 +1,13 @@
-import { fromJSON } from 'postcss'
-
 export {default as Avatar } from './Avatar'
 
 //exporting Button Component
 export { default as Button } from './Button'
 
 // exporting card component
-export { default as Card } from './Card/Card'
+export { default as Card } from './Card'
 export { default as CardBody } from './Card/CardBody'
 export { default as CardImage } from './Card/CardImage'
+export { default as CardSubTitle } from './Card/CardSubTitile'
 export { default as CardTitle } from './Card/CardTitle'
 export { default as CardText } from './Card/CardText'
 
