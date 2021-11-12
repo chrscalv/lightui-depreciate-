5bd7ccbd314811876be05215425872e61709a204
chrscalv <chrscalvn@gmail.com>
2021-11-12 11:19:13 +0700

    updating index component

--- components/index.js
+++ components/index.js
@@ -1,5 +1,15 @@
+import { fromJSON } from 'postcss'
+
 export {default as Avatar } from './Avatar'
 
+//exporting Button Component
 export { default as Button } from './Button'
+
+// exporting card component
+export { default as Card } from './Card/Card'
+export { default as CardBody } from './Card/CardBody'
+export { default as CardImage } from './Card/CardImage'
+export { default as CardTitle } from './Card/CardTitle'
+export { default as CardText } from './Card/CardText'
 
 export { default as Container } from './Container'
